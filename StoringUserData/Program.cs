f25395510a8f485260df54e1189a83c409d816fd
Lincon Avila <lincon1000ls@gmail.com>
2024-11-24 00:32:31 -0300

    Add a simple storing user data exercise

---
+++ StoringUserData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringUserData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Aba";
+            var number = "0123456789";
+            int age = 23;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
